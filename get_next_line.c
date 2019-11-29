8cd653a6a09d0970d0a679cd88cb8e47259e0a07
Kévin PARIS <kevin.paris701@gmail.com>
2019-11-29 14:09:58 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -30,43 +30,55 @@ char	*ft_strchr(const char *str, int c)
 	return (&str2[i]);
 }
 
+int	get_next_line3(t_struct *info, int fd)
+{
+	if (!(info->buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	info->nb_read = read(fd, info->buf, BUFFER_SIZE);
+	info->buf[info->nb_read] = 0;
+	info->str = ft_strjoin(info->str, info->buf);
+	info->buf = 0;
+	return(0);
+}
+
+int		get_next_line2(t_struct *info, int fd, char **line)
+{
+	size_t	i;
+
+	i = 0;
+	while (ft_strchr(info->str, '\n') == NULL && info->nb_read != 0)
+		if (get_next_line3(info, fd) == -1)
+			return (-1);
+	if (ft_strchr(info->str, '\n') != NULL)
+	{
+		while (i < ft_strlen(info->str) && info->str[i] != '\n')
+			i += 1;
+		*line = i == 0 ? ft_strdup("") : ft_substr(info->str, 0, i);
+		info->str = ft_strdup(&info->str[i + 1]);
+		return (1);
+	}
+	else
+	{
+		*line = ft_strdup(info->str);
+		free(info->str);
+		info->str = 0;
+		return (0);
+	}
+}
+
 int		get_next_line(int fd, char **line)
 {
 	static t_struct	info;
-	size_t			i;
 
-	i = 0;
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-
 	if (!info.str)
 	{
 		info.str = ft_strdup("");
 		info.nb_read = 1;
 	}
 	while (info.nb_read > 0 || ft_strchr(info.str, '\n') != NULL)
-	{
-		while (ft_strchr(info.str, '\n') == NULL && info.nb_read != 0)
-		{
-			if (!(info.buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
-				return (-1);
-			info.nb_read = read(fd, info.buf, BUFFER_SIZE);
-			info.buf[info.nb_read] = 0;
-			info.str = ft_strjoin(info.str, info.buf);
-			free(info.buf);
-		}
-		while (i < ft_strlen(info.str) && info.str[i] != '\n')
-			i += 1;
-		*line = i == 0 ? ft_strdup("") : ft_substr(info.str, 0, i);
-		info.str = i == 0 ? ft_strdup("") : ft_strdup(&info.str[i + 1]);
-		if (info.nb_read == 0)
-		{
-			free(info.str);
-			info.str = 0;
-			return (0);
-		}
-		return (1);
-	}
+		return(get_next_line2(&info, fd, line));
 	*line = ft_strdup(info.str);
 	free(info.str);
 	info.str = 0;
